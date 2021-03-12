e24692e7c42c1f5a3a5f23b1d1d8888602a31626
T-Mook <msp770@gmail.com>
2021-03-12 20:41:55 +0900

    [INFRA] 기초 layout 세팅 & 불필요 pages, component 제거

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,91 +1,32 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+  <v-app>
+    <v-app-bar :color="appBar.color" dense flat>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-app-bar-title class="white--text">
+        <small>{{ appBar.title }}</small>
+      </v-app-bar-title>
+      <v-spacer />
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <v-container class="mb-4">
+      <nuxt />
+    </v-container>
+    <main-footer />
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
+<script lang="ts">
+import { Component, Provide, Vue } from 'nuxt-property-decorator'
+import MainFooter from '@/components/default/MainFooter.vue'
+@Component({
+  components: {
+    MainFooter,
   },
+})
+class LayoutDefault extends Vue {
+  @Provide() appBar: { title: string; color: string } = {
+    title: '다이슨 스피어 프로그램 공장 최적화 도우미',
+    color: 'rgba(21,31,32,1)',
+  }
 }
+export default LayoutDefault
 </script>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,38 +1,33 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <h1 v-if="error.statusCode === 404">
       {{ pageNotFound }}
     </h1>
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <NuxtLink to="/"> Home page</NuxtLink>
   </v-app>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { Component, Prop, Provide, Vue } from 'nuxt-property-decorator'
+@Component({
   layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null,
-    },
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    }
-  },
-  head() {
-    const title =
+})
+class LayoutError extends Vue {
+  @Prop({ type: Object, default: null }) error!: { statusCode: number }
+  @Provide() pageNotFound: string = '존재하지 않는 페이지'
+  @Provide() otherError: string = '알 수 없는 에러'
+  head(): object {
+    const title: string =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
       title,
     }
-  },
+  }
 }
+export default LayoutError
 </script>
 
 <style scoped>
