cdd5890a01f687f5dffb881539020480b293e00a
T-Mook <msp770@gmail.com>
2021-03-12 21:01:59 +0900

    [FEATURE] 채널 링크 추가 in MainFooter.vue

--- components/default/MainFooter.vue
+++ components/default/MainFooter.vue
@@ -2,7 +2,10 @@
   <v-footer :color="color" padless>
     <v-row justify="center" no-gutters>
       <v-col class="py-4 text-center grey--text" cols="12">
-        {{ contact }}
+        contact:
+        <a href="https://www.youtube.com/c/TMook" target="_blank">{{
+          contact
+        }}</a>
       </v-col>
     </v-row>
   </v-footer>
@@ -12,7 +15,7 @@
 import { Component, Provide, Vue } from 'nuxt-property-decorator'
 @Component({})
 class ComponentsDefaultMainFooter extends Vue {
-  @Provide() contact: string = 'contact : msp770@gmail.com'
+  @Provide() contact: string = 'TMook'
   @Provide() color: string = 'rgba(21,31,32,1)'
 }
 export default ComponentsDefaultMainFooter
