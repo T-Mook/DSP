f3b0304c97db49dbb2ba4630f5f16e44f124c312
T-Mook <msp770@gmail.com>
2021-03-13 15:44:59 +0900

    [FEATURE] dataEn.json 샘플 파일 추가 및 화면상 출력 테스트

--- components/index/RequiredProcess.vue
+++ components/index/RequiredProcess.vue
@@ -1,14 +1,25 @@
 <template>
-  <v-card>{{ resourceName }}</v-card>
+  <v-card>
+    {{ resourceName }}
+    {{ test }}
+  </v-card>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'nuxt-property-decorator'
+<script>
+import dataEnSample from '~/static/data/dataEn.json'
 
-@Component({})
-class ComponentsIndexRequiredProcess extends Vue {
-  @Prop({ type: String, default: 'Electric Motor' }) resourceName!: string
+export default {
+  props: {
+    resourceName: { type: String, default: 'Electric Motor' },
+  },
+  asyncData() {
+    return { dataEnSample }
+  },
+  computed: {
+    test() {
+      const result = dataEnSample[0]['Iron Ore']
+      return result
+    },
+  },
 }
-
-export default ComponentsIndexRequiredProcess
 </script>
