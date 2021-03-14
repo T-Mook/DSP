07d64a97d9bcd3d582a06ccfe6c085eb2af263d7
T-Mook <msp770@gmail.com>
2021-03-14 18:08:57 +0900

    [FIX] lint conflicts 해소

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     'vue/singleline-html-element-content-newline': 'off', // https://eslint.vuejs.org/rules/singleline-html-element-content-newline.html
     'vue/no-v-html': 'off', // This rule reports all uses of v-html directive in order to reduce the risk of injecting potentially unsafe / unescaped html into the browser leading to Cross-Site Scripting (XSS) attacks.
+    'dot-notation': 'off',
     'comma-dangle': ['error', 'always-multiline'], // *conlict prettier, ref : https://eslint.org/docs/rules/comma-dangle
     'space-before-function-paren': ['error', 'never'], // *conlict prettier, ref : https://eslint.org/docs/rules/space-before-function-paren
   },
