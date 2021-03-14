b92b31e87d19b7fb1b1d5308994b26ce699fafa7
T-Mook <msp770@gmail.com>
2021-03-15 02:18:35 +0900

    [FIX] 더이상 세부 레시피가 존재하지 않는 경우 error

--- components/index/RequiredProcess.vue
+++ components/index/RequiredProcess.vue
@@ -101,7 +101,7 @@ class ComponentsIndexRequiredProcess extends Vue {
     return resultArray
   }
 
-  returnLowerMaterialProductionInfoArray(
+  inputLowerMaterialProductionInfoArray(
     neededList: Array<{ [key: string]: any }>,
   ): Array<object> {
     const resultNeededList: Array<object> = []
@@ -136,65 +136,67 @@ class ComponentsIndexRequiredProcess extends Vue {
 
   returnNeededList(targetResourceName: string): Array<object> {
     const datas: { [key: string]: any } = dataEnSample
-    // const totalNameOfResource: Array<string> = Object.keys(datas)
-    // if (targetResourceName in totalNameOfRes
 
-    const totalTextOfRecipe: any = datas[targetResourceName]['recipe']
-    const arrayOfRecipe: Array<string> = this.arrayAfterSlice(
-      totalTextOfRecipe,
-      '&',
-    )
-    const recipeList: Array<object> = this.returnObjectFromEachRecipe(
-      arrayOfRecipe,
-    )
-    // Handling Needed Buildings
-    const textOfBuildingName: string = datas[targetResourceName]['producedIn']
-    const buildingList: Array<string> = this.arrayAfterSlice(
-      textOfBuildingName,
-      '&',
-    )
-    // Handling Output
-    const textOfOutputNumber: string = datas[targetResourceName]['output']
-    const outputList: Array<string> = this.arrayAfterSlice(
-      textOfOutputNumber,
-      '&',
-    )
-    // Handling Produce Time
-    const textOfProducedSecond: string =
-      datas[targetResourceName]['producedSecond']
-    const secondList: Array<string> = this.arrayAfterSlice(
-      textOfProducedSecond,
-      '&',
-    )
-    // Check Value Number of Each List
-    const ArrayOfListsLength: Array<number> = [
-      recipeList.length,
-      buildingList.length,
-      outputList.length,
-      secondList.length,
-    ]
-    if (ArrayOfListsLength.every((v) => v !== ArrayOfListsLength[0])) {
-      // Check total condition in array
-      const text: string = '목록 숫자가 맞지 않습니다'
-      throw text
+    const totalNameOfResource: Array<string> = Object.keys(datas)
+    if (totalNameOfResource.includes(targetResourceName)) {
+      const totalTextOfRecipe: any = datas[targetResourceName]['recipe']
+      const arrayOfRecipe: Array<string> = this.arrayAfterSlice(
+        totalTextOfRecipe,
+        '&',
+      )
+      const recipeList: Array<object> = this.returnObjectFromEachRecipe(
+        arrayOfRecipe,
+      )
+      // Handling Needed Buildings
+      const textOfBuildingName: string = datas[targetResourceName]['producedIn']
+      const buildingList: Array<string> = this.arrayAfterSlice(
+        textOfBuildingName,
+        '&',
+      )
+      // Handling Output
+      const textOfOutputNumber: string = datas[targetResourceName]['output']
+      const outputList: Array<string> = this.arrayAfterSlice(
+        textOfOutputNumber,
+        '&',
+      )
+      // Handling Produce Time
+      const textOfProducedSecond: string =
+        datas[targetResourceName]['producedSecond']
+      const secondList: Array<string> = this.arrayAfterSlice(
+        textOfProducedSecond,
+        '&',
+      )
+      // Check Value Number of Each List
+      const ArrayOfListsLength: Array<number> = [
+        recipeList.length,
+        buildingList.length,
+        outputList.length,
+        secondList.length,
+      ]
+      if (ArrayOfListsLength.every((v) => v !== ArrayOfListsLength[0])) {
+        // Check total condition in array
+        const text: string = '목록 숫자가 맞지 않습니다'
+        throw text
+      }
+      // Change Array to Each Building-Recipe-Output-Second Array
+      const arrayOfLists: { [key: string]: any } = {
+        buildingList,
+        recipeList,
+        outputList,
+        secondList,
+      }
+      const neededList = this.makeEachRecipeArray(arrayOfLists)
+
+      return neededList
+    } else {
+      return []
     }
-    // Change Array to Each Building-Recipe-Output-Second Array
-    const arrayOfLists: { [key: string]: any } = {
-      buildingList,
-      recipeList,
-      outputList,
-      secondList,
-    }
-    const neededList = this.makeEachRecipeArray(arrayOfLists)
-    return neededList
   }
 
   requiredBuildingAndRecipe(targetResourceName: string): object | undefined {
     try {
       const neededList = this.returnNeededList(targetResourceName)
-      const resultArray = this.returnLowerMaterialProductionInfoArray(
-        neededList,
-      )
+      const resultArray = this.inputLowerMaterialProductionInfoArray(neededList)
 
       return resultArray
     } catch (e) {
